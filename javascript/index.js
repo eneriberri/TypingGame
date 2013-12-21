d1d9cb25dbdb3e78cb99046c18921582467e4220
Irene Nguyen <irene.m.nguyen@gmail.com>
2013-12-21 02:53:33 -0600

    created a timer

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
   $('#type-box').focus();
+  $('.timer').text("00:00");
+  var sec = 0;
+  window.setInterval(function() {     
+    if(sec === 60) sec = 0;
+    var secStr = sec < 10 ? "0" + sec : "" + sec;
+    $('.timer').text("00:" + secStr); 
+    sec++;
+  }, 1000);
+
+  
   
   var prompt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " + 
   "Mauris sed tortor imperdiet, ultrices est ut, auctor neque. In ligula lacus, " + 
@@ -8,34 +18,31 @@ $(document).ready(function() {
 
   var promptArr = prompt.split("");
   promptArr.shift();
-  promptArr.unshift("<span class='highlight'>" + "L" + "</span>")
+  promptArr.unshift("<span class='highlight'>" + prompt[0] + "</span>")
   $('.prompt').append(promptArr.join(""));
+  
   
   var i = 0;
   $('#type-box').keypress(function(event) 
   {  
-    var rgxSpan = /<\w+\s+\w+='\w+'>/;
-    var rgxSpanEnd = /<\/\w+>/;
-    var str = promptArr[i];
-    str = str.replace(rgxSpan, "");
-    str = str.replace(rgxSpanEnd, "");
+    if(i !== promptArr.length - 1) { //unbind event
     
-    if(event.keyCode === prompt.charCodeAt(i)) {
-      promptArr[i] = str;
-
-      promptArr[i+1] = "<span class='highlight'>" + promptArr[i+1] + "</span>";
+      var rgxSpan = /<\w+\s+\w+='\w+'>/;
+      var rgxSpanEnd = /<\/\w+>/;
+      promptArr[i] = promptArr[i].replace(rgxSpan, "").replace(rgxSpanEnd, "");
+    
+      if(event.keyCode === prompt.charCodeAt(i)) {
+        promptArr[i+1] = "<span class='highlight'>" 
+                         + promptArr[i+1] + "</span>";    
+        i++;
+      }
+      else {
+        promptArr[i] = "<span class='error'>" 
+                       + promptArr[i] + "</span>";
+      }  
+    
       promptStr = promptArr.join("");
       $('.prompt').html(promptStr);
-    
-      i++;
     }
-    else {
-      promptArr[i] = str;
-      promptArr[i] = "<span class='error'>" + promptArr[i] + "</span>";
-      promptStr = promptArr.join("");
-      $('.prompt').html(promptStr);
-      // $('.prompt').addClass('highlight-error');
-      // $('.prompt').removeClass('highlight');
-    }  
   });
 });
